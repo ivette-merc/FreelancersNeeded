54de2eb20fe319afeddb8c31e610863708ec960a
ivette-merc <ivettemerc@gmail.com>
2024-01-22 17:30:48 -0500

    workshop done

--- javascript.js
+++ javascript.js
@@ -1,35 +1,98 @@
-//Create array or object with freelancer name, prices and occupation
-const freelancers = [
-  { name: "Billy_Loomis", price: 30, occupation: "Handyman" },
-  { name: "Patrick_Bateman", price: 70, occupation: "Financial_Advisor" },
-  { name: "Jack_Torrance", price: 40, occupation: "Copy_Writer" },
-  { name: "Hannibal_Lecter", price: 60, occupation: "Therapist" },
-  { name: "Freddy_Krueger", price: 60, occupation: "Sleep_Therapist" },
-  { name: "Jason_Voorhees", price: 20, occupation: "Camp_Counselor" },
-  { name: "Michael_Myers", price: 15, occupation: "Dog_Walker" },
-  { name: "Charles_Lee_Ray", price: 50, occupation: "Doll_Maker" },
-  { name: "Tiffany_Valentine", price: 35, occupation: "Fashion_Consultant" },
-  { name: "Annie_Wilkes", price: 40, occupation: "Home_Health_Aid" },
-  { name: "Carrie_White", price: 15, occupation: "High_School_Tutor" },
+const names = [
+  "Pactrick Bateman",
+  "Carrie White",
+  "Jack Torrance",
+  "Hannibal Lecter",
+  "Jason Voorhees",
+  "Michael Myers",
+  "Charles Lee Ray",
+  "Tiffany Valentine",
+  "Annie Wilkes",
 ];
+
+const occupations = [
+  "Tutor",
+  "Food Critic",
+  "Home Health Aid",
+  "Lanscaping",
+  "Plumber",
+  "Copy Writer",
+  "Stylist",
+  "Makeup Artist",
+  "Caterer",
+];
+
+const prices = [40, 50, 35, 50];
+
 const maxFreelancers = 11;
 
+const listFreelancer = [
+  { name: "Billy Loomis", price: 30, occupation: "Handyman" },
+  { name: "Freddy Krueger", price: 60, occupation: "Sleep Therapist" },
+];
+
+function calculateAveragePrice() {
+  const total = listFreelancer.reduce(
+    (sum, freelancer) => sum + freelancer.price,
+    0
+  );
+  return listFreelancer.length === 0 ? 0 : total / listFreelancer.length;
+}
+
+const addFreelancerIntervalId = setInterval(addFreelancer, 500);
+
+render();
 
 
 
-//function to find mean of price
-// function calculateMeanPrice(freelancers) {
-//   if (freelancers.length === 0) {
-//     return 0; // Avoid division by zero
-//   }
+function render() {
+  const freelancersId = document.querySelector("#freelancers");
 
-//   const totalPrices = freelancers.reduce(
-//     (sum, freelancers) => sum + freelancers.price,
-//     0
-//   );
-//   const meanPrice = Math.round(totalPrices / freelancers.length);
+  freelancersId.innerHTML = `<tr>
+      <th>Name</th>
+      <th>Occupation</th>
+      <th>Starting Price</th>
+     </tr>`;
 
-//   return meanPrice;
-// }
-// const meanPrice = calculateMeanPrice(freelancers);
-// console.log("Mean Price:", meanPrice);
+  listFreelancer.forEach((freelancer) => {
+    const tr = document.createElement("tr");
+    const tdName = document.createElement("td");
+    tdName.textContent = freelancer.name;
+
+    const tdOccupation = document.createElement("td");
+    tdOccupation.textContent = freelancer.occupation;
+
+    const tdPrice = document.createElement("td");
+    tdPrice.classList.add("price");
+    tdPrice.textContent = `$${freelancer.price}`;
+
+    tr.appendChild(tdName);
+    tr.appendChild(tdOccupation);
+    tr.appendChild(tdPrice);
+
+    freelancersId.appendChild(tr);
+
+    const average = document.querySelector("#average");
+    average.innerHTML = `The average price is: $${calculateAveragePrice().toFixed()}`;
+  });
+}
+
+document.getElementById("#freelancers").style.justifyItems = "center";
+
+function addFreelancer() {
+  const name = names[Math.floor(Math.random() * names.length)];
+  const price = prices[Math.floor(Math.random() * prices.length)];
+  const occupation =
+    occupations[Math.floor(Math.random() * occupations.length)];
+
+  listFreelancer.push({ name, price, occupation });
+
+  render();
+
+  
+
+  if (listFreelancer.length >= maxFreelancers) {
+    clearInterval(addFreelancerIntervalId);
+  }
+}
+
